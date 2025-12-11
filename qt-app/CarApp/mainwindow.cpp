c811ee418948a5fea743c75b9c10706cbf825191
Niilo Illikainen <niilo.illikainen22@gmail.com>
2025-12-11 21:04:09 +0200

    UI parannusta

--- qt-app/CarApp/mainwindow.cpp
+++ qt-app/CarApp/mainwindow.cpp
@@ -7,6 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    ui->tableWidgetCars->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
     manager = new QNetworkAccessManager(this);
 
     connect(manager, &QNetworkAccessManager::finished,
